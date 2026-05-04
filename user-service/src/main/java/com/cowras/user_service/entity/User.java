4670dbf2e164eb2d8b02a54ff28a36f2ab22aacc
arbindgitlab <arbind.sharma2@gmail.com>
2026-05-05 02:43:50 +0530

    check in the basic set up for user service

---
+++ user-service/src/main/java/com/cowras/user_service/entity/User.java
@@ -0,0 +1,28 @@
+package com.cowras.user_service.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    // e.g. "ADMIN", "USER"
+    @Column(nullable = false)
+    private String role;
+
+    private String email;
+}
