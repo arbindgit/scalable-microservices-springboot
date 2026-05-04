57a2a45cb52ae70877cae126417c163b16641969
arbindgitlab <arbind.sharma2@gmail.com>
2026-05-05 02:01:32 +0530

    commiting basic code structure

---
+++ user-service/user-service/src/test/java/com/cowras/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cowras.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
